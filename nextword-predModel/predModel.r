32839dd02c20a084a3be37fbd34d810e182d7504
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2018-02-20 21:21:43 +0200

    import ngrams files and create taggers

--- nextword-predModel/predModel.r
+++ nextword-predModel/predModel.r
@@ -2,3 +2,25 @@ library(stringr); library(data.table)
 library(openNLP); library(NLP)
 library(tm)
 
+# import n-grams files
+unigram.dt <- readRDS("data/unigram.rds")
+bigram.dt <- readRDS("data/bigram.rds")
+trigram.dt <- readRDS("data/trigram.rds")
+badwords <- readRDS("data/badwords.rds")
+
+# speech tagging Annotators
+sent.token.annotator <- Maxent_Sent_Token_Annotator()
+word.token.annotator <- Maxent_Word_Token_Annotator()
+pos.tag.annotator <- Maxent_POS_Tag_Annotator()
+
+
+
+
+
+
+
+
+
+
+
+
