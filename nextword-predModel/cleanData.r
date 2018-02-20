de5ed344f919992158f522ea091074bea5d71b9c
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2018-02-20 20:26:11 +0200

    created file for claening the dara and added the required packages for the cleaning

---
+++ nextword-predModel/cleanData.r
@@ -0,0 +1,9 @@
+# Load the required data for cleaning data
+library(NLP); library(tm);library(dplyr)
+library(RColorBrewer);library(wordcloud); library(stringi)
+library(ggplot2); library(knitr);library(RWeka) 
+library(gridExtra); library(magrittr); library(SnowballC)
+library(doParallel); registerDoParallel(makeCluster(4))
+library(stringr); library(dplyr); library(caret)
+library(tau); library(data.table)
+
